30ea2ee8f0f261719dfe6418af45041d168590aa
Raphael Kubo da Costa <kubito@gmail.com>
2011-04-14 23:58:40 -0300

    Sync FindExiv2.cmake and FindXine.cmake with kdelibs.
    FindPackageHandleStandardArgs.cmake had to be imported too, as strigi
    currently requires CMake 2.6+, and the new syntax for the call has
    been added in CMake 2.8.
    
    FindPackageHandleStandardArgs.cmake is from revision 119344a.
    FindExiv2.cmake is from revision e09120e.
    FindXine.cmake is from revision 8f8ddd1.
    
    The upated modules, besides being cleaner, also do not call pkgconfig
    the old-fashioned way, which should fix SourceForge tracker bug 3149288.

--- cmake/FindXine.cmake
+++ cmake/FindXine.cmake
@@ -8,7 +8,6 @@
 #  XINE_LIBRARY - The libraries needed to use XINE
 #  XINE_XCB_FOUND - libxine can use XCB for video output
 
-# Copyright (c) 2008 Helio Chissini de Castro, <helio@kde.org>
 # Copyright (c) 2006,2007 Laurent Montel, <montel@kde.org>
 # Copyright (c) 2006, Matthias Kretz, <kretz@kde.org>
 #
@@ -20,35 +19,46 @@ if (XINE_INCLUDE_DIR AND XINE_LIBRARY)
   set(Xine_FIND_QUIETLY TRUE)
 endif (XINE_INCLUDE_DIR AND XINE_LIBRARY)
 
-IF (NOT WIN32)
-    FIND_PACKAGE(PkgConfig)
-	PKG_CHECK_MODULES(PKG_XINE libxine)
-ENDIF (NOT WIN32)
+find_package(PkgConfig)
+if (PKG_CONFIG_FOUND)
+   pkg_check_modules(PC_LIBXINE QUIET libxine)
+endif (PKG_CONFIG_FOUND)
 
-FIND_PATH(XINE_INCLUDE_DIR NAMES xine.h
-    PATHS ${PKG_XINE_INCLUDE_DIRS} )
+find_path(XINE_INCLUDE_DIR NAMES xine.h 
+    HINTS                    # HINTS is new in cmake 2.6. These dirs will be preferred over the default ones
+    ${PC_LIBXINE_INCLUDEDIR} 
+    ${PC_LIBXINE_INCLUDE_DIRS} 
+    )
 
-FIND_LIBRARY(XINE_LIBRARY NAMES xine
- PATHS ${PKG_XINE_LIBRARY_DIRS} )
+find_library(XINE_LIBRARY NAMES xine
+ HINTS
+  ${PC_LIBXINE_LIBDIR}
+  ${PC_LIBXINE_LIBRARY_DIRS}
+ )
 
-if (XINE_INCLUDE_DIR AND XINE_LIBRARY)
-      set(XINE_FOUND TRUE)
-      string(REGEX REPLACE "[0-9].[0-9]." "" XINE_BUGFIX_VERSION ${PKG_XINE_VERSION})
-	  set(XINE_VERSION ${PKG_XINE_VERSION})
-endif (XINE_INCLUDE_DIR AND XINE_LIBRARY)
+find_program(XINECONFIG_EXECUTABLE NAMES xine-config 
+   HINTS
+   ${PC_LIBXINE_PREFIX}/bin
+)
+
+if (XINE_INCLUDE_DIR AND XINE_LIBRARY AND XINECONFIG_EXECUTABLE)
+   exec_program(${XINECONFIG_EXECUTABLE} ARGS --version RETURN_VALUE _return_VALUE OUTPUT_VARIABLE XINE_VERSION)
+   if("${XINE_VERSION}" VERSION_GREATER "1.1.0")   #if (... VERSION_GREATER) is new since cmake 2.6.2
+      set(XINE_VERSION_OK TRUE)
+      string(REGEX REPLACE "[0-9]\\.[0-9]\\." "" XINE_BUGFIX_VERSION ${XINE_VERSION})
+   endif("${XINE_VERSION}" VERSION_GREATER "1.1.0")
+endif (XINE_INCLUDE_DIR AND XINE_LIBRARY AND XINECONFIG_EXECUTABLE)
 
 
-if( XINE_FOUND )
-  INCLUDE(CheckCSourceCompiles)
-  SET(CMAKE_REQUIRED_INCLUDES ${XINE_INCLUDE_DIR})
-  SET(CMAKE_REQUIRED_LIBRARIES ${XINE_LIBRARY})
-  CHECK_C_SOURCE_COMPILES("#include <xine.h>\nint main()\n{\n  xine_open_video_driver(xine_new(), \"auto\", XINE_VISUAL_TYPE_XCB, NULL);\n  return 0;\n}\n" XINE_XCB_FOUND)
-endif(XINE_FOUND)
+if( XINE_VERSION_OK)
+  include(CheckCSourceCompiles)
+  set(CMAKE_REQUIRED_INCLUDES ${XINE_INCLUDE_DIR})
+  set(CMAKE_REQUIRED_LIBRARIES ${XINE_LIBRARY})
+  check_c_source_compiles("#include <xine.h>\nint main()\n{\n  xine_open_video_driver(xine_new(), \"auto\", XINE_VISUAL_TYPE_XCB, NULL);\n  return 0;\n}\n" XINE_XCB_FOUND)
+  set(XINE_FOUND TRUE)
+endif(XINE_VERSION_OK)
 
-if (XINE_FOUND)
-   if (NOT Xine_FIND_QUIETLY)
-      message(STATUS "Found XINE: ${XINE_LIBRARY}")
-   endif (NOT Xine_FIND_QUIETLY)
-endif (XINE_FOUND)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Xine  "Could NOT find XINE 1.1.1 or greater"  XINE_INCLUDE_DIR XINE_LIBRARY XINECONFIG_EXECUTABLE  XINE_VERSION_OK)
 
-MARK_AS_ADVANCED(XINE_INCLUDE_DIR XINE_LIBRARY)
+mark_as_advanced(XINE_INCLUDE_DIR XINE_LIBRARY XINECONFIG_EXECUTABLE)
